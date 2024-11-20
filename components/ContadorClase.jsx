c53c96cf7d491fddd8cf39ef84bb71c42a2903a1
Diego Gomez <diegocor4@gmail.com>
2024-11-20 11:56:11 -0300

    metodos incrementar y reducir

--- components/ContadorClase.jsx
+++ components/ContadorClase.jsx
@@ -2,8 +2,20 @@ import React,{Component} from "react";
 
 class ContadorClase extends Component {
     constructor(props){
+        super(props);
+        this.state = {
+            conteo: 0,
+        };
 
     }
+
+    incrementar = () => {
+        this.setState((prevState) => ({ conteo: prevState.conteo + 1}));
+    };
+
+    reducir = () => {
+        this.setState((prevState) => ({ conteo: prevState.conteo - 1}));
+    };
 
     render(){
 
