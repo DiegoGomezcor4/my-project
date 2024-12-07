064d26cb910d697cca80e8284e8b882c1cb44bc1
Diego Gomez <diegocor4@gmail.com>
2024-12-07 12:50:27 -0300

    refactorizado componente de clase a componenete de funcion

--- components/ContadorClase.jsx
+++ components/ContadorClase.jsx
@@ -1,38 +1,26 @@
-import React,{Component} from "react";
+import React,{ useState } from "react";
 
+const ContadorClase = ()=>{
+    const [conteo, setConteo] = useState(0);
 
-
-class ContadorClase extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            conteo: 0,
-        };
-
-    }
-
-    incrementar = () => {
-        this.setState((prevState) => ({ conteo: prevState.conteo + 1}));
+    const incrementar = () => {
+        setConteo((prevConteo) => prevConteo + 1);
     };
 
-    reducir = () => {
-        this.setState((prevState) => ({ conteo: prevState.conteo - 1}));
+    const reducir = () => {
+        setConteo((prevConteo) => prevConteo - 1);
     };
 
-    render(){
-
-        const { conteo } = this.state;
-
-        return (
-            <div>
-                <h1>Contador de clicks</h1>
-                <p>conteo:{conteo}</p>
-                <button onClick={this.incrementar}> Sumar Click </button>
-                <button onClick={this.reducir}>Restar Click</button>
-            </div>
-        );
-
-    }
+    return(
+        <div>
+            <h1>Contador de cliks</h1>
+            <p>conteo: {conteo}</p>
+            <button onClick={incrementar}> Sumar Click</button>
+            <button onClick={reducir}>Restar Click</button>
+        </div>
+    )
 }
 
-export default ContadorClase
+
+
+export default ContadorClase;
